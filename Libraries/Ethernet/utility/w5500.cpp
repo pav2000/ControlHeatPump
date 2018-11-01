4218c01bbd33bdaa1ad6d44cb05724e170922024
Vadim <vad7@yahoo.com>
2018-11-01 09:36:14 +0300

    Фикс ошибки компиляции на Arduino 1.8.7

--- Libraries/Ethernet/utility/w5500.cpp
+++ Libraries/Ethernet/utility/w5500.cpp
@@ -147,7 +147,7 @@ digitalWriteDirectARM(SS,LOW);
 
 void W5500Class::execCmdSn(SOCKET s, SockCMD _cmd) {
     writeSnCR(s, _cmd);     // Send command to socket
-    uint32_t t = GetTickCount();
-    while(readSnCR(s)) if(GetTickCount() - t > W5500_EXEC_CMD_TIMEOUT) break;   // Wait for command to complete
+//    uint32_t t = GetTickCount();
+    while(readSnCR(s)) ; //if(GetTickCount() - t > W5500_EXEC_CMD_TIMEOUT) break;   // Wait for command to complete
 }
 #endif
