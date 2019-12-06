a6fbf13f93eb20a11a1f656be9e3bc04f6b57ac6
pav2000 <firstlast2007@gmail.com>
2019-12-06 11:23:25 +0300

    Теперь в режиме супербойлер циркуляция ГВС не включается. Без супербойлера включается всегда.

--- Control/Control.ino
+++ Control/Control.ino
@@ -1078,7 +1078,13 @@ void vReadSensor_delay8ms(int16_t ms8)
 #endif  // #ifndef SUPERBOILER 
 						if (HP.get_Circulation())                                               // Циркуляция разрешена
 						{
-							if ((HP.dRelay[RCOMP].get_Relay()||HP.dFC.isfOnOff())&&(HP.get_onBoiler())) { HP.dRelay[RPUMPB].set_ON(); goto delayTask;/* continue;*/} // идет нагрев ГВС включаем насос ГВС ВСЕГДА - улучшаем перемешивание
+			                if ((HP.dRelay[RCOMP].get_Relay()||HP.dFC.isfOnOff())&&(HP.get_onBoiler())) { // идет нагрев ГВС 
+			                #ifdef SUPERBOILER  	
+                               if (HP.dRelay[RSUPERBOILER].get_Relay()) {HP.dRelay[RPUMPB].set_OFF(); goto delayTask;/* continue;*/} // идет прямой нагрев ГВС через предконденсатор, насос циркуляции ВЫКЛЮЧАЕМ
+                               else    
+                            #endif
+                               {HP.dRelay[RPUMPB].set_ON();goto delayTask;} // идет нагрев ГВС включаем насос циркуляции ВСЕГДА - улучшаем перемешивание     
+			                } 
 							if (HP.get_CirculWork()==0) { HP.dRelay[RPUMPB].set_OFF(); goto delayTask;/* continue;*/}   // В условиях стоит время работы 0 - выключаем насос ГВС
 							if (HP.get_CirculPause()==0) { HP.dRelay[RPUMPB].set_ON(); goto delayTask;/* continue;*/}  // В условиях стоит время паузы 0 - включаем насос ГВС
 							if(HP.dRelay[RPUMPB].get_Relay())                                       // Насос включен Смотрим времена
