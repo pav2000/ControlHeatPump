ff4bd10351d74df55e90258ce583f3cde43e0578
Vadim <vad7@yahoo.com>
2020-08-31 19:42:09 +0300

    upd

--- Control/Nextion.ino
+++ Control/Nextion.ino
@@ -351,8 +351,9 @@ void Nextion::readCommand()
 			return;
 //		case 0x00:	// Nextion has started or reset
 //		case 0x67:  // Touch Coordinate (awake)
-//		case 0x68:  // Touch Coordinate (sleep)
-//			break;
+		case 0x68:  // Touch Coordinate (sleep)
+			fUpdate = 1;
+			break;
 //		default: // 0x00 - 	Invalid Instruction, 0x03 - Invalid Page ID, 0x1A,0x1B - Invalid Variable, 0x1E - Invalid Quantity of Parameters, 0x1F - IO Operation failed
 		}
 	}
